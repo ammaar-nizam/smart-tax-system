aab30cf77ed5e57c64d774e4bb4e70aa5992a229
ammaar-nizam <ammaar.nizam@outlook.com>
2024-05-04 08:49:24 +0530

    Merge changes from master branch

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -10,20 +10,18 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="app">
-      <Sidebar/>
-      <main className="content">
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Website />} />
-          <Route path="/services" element={<Services/>} />
-          <Route path="/guidance" element={<Guidance/>} />
-          <Route path="/feedback" element={<Feedback/>} />
-          <Route path="/estate-duty-tax" element={<EstateDutyTax/>} />
-        </Route>
-      </Routes>
-      </main>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Website />} />
+            <Route path="/services" element={<Services/>} />
+            <Route path="/guidance" element={<Guidance/>} />
+            <Route path="/feedback" element={<Feedback/>} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
